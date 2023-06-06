15127cdbbadd5350b12cb4c5fed822f2e1151807
German Varela <germanvarela85@gmail.com>
2023-06-06 13:34:20 -0300

    init commit

---
+++ contacto/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+class FormularioContacto(forms.Form):
+    nombre = forms.CharField(label="Nombre", required=True, max_length=40)
+    email = forms.CharField(label="Email", required=True)
+    contenido = forms.CharField(label="Contenido", widget=forms.Textarea)
+    
